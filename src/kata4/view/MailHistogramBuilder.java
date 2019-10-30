20ab2352b8a90dac566ff6487ae9160ab1330a64
Edgar Holzmann <e.holzmann.bvb@googlemail.com>
2019-10-30 09:50:06 +0000

    complete the implementation for build the histogram of emails.

--- src/kata4/view/MailHistogramBuilder.java
+++ src/kata4/view/MailHistogramBuilder.java
@@ -1,5 +1,17 @@
 package kata4.view;
 
+
+import java.util.List;
+import kata4.model.Histograma;
+import kata4.model.Mail;
+
 public class MailHistogramBuilder {
-    
+    public static Histograma build(List<Mail> mailList){
+        Histograma<String> histogram = new Histograma();
+        
+        for(Mail mail: mailList){
+            histogram.increment(mail.getDomain());
+        }
+        return histogram;
+    }
 }
