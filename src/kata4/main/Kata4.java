e6d7a9e14d5cc46520066e32b8919aeb668c5649
Edgar Holzmann <e.holzmann.bvb@googlemail.com>
2019-10-30 10:03:06 +0000

    complete the implementation of Main for show the results.

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -1,25 +1,21 @@
 package kata4.main;
 
+import java.util.List;
 import kata4.model.Histograma;
+import kata4.model.Mail;
 import kata4.view.HistogramDisplay;
+import kata4.view.MailHistogramBuilder;
+import kata4.view.MailListReader;
 
 public class Kata4 {
 
     public static void main(String[] args) {
-        Histograma<String> histogram = new Histograma<>();
+        String fileName = new String("email.txt");
+        List<Mail> mailList = MailListReader.read(fileName);
+        Histograma histogram =  MailHistogramBuilder.build(mailList);
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
         
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("dis.ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("gmail.com");
-        histogram.increment("dis.ulpgc.es");
-        histogram.increment("gmail.com");
-        
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
     }
     
 }
