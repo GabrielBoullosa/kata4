2554af9be241e2ead4ebfb13165fd63eaba1117c
Edgar Holzmann <e.holzmann.bvb@googlemail.com>
2019-10-30 09:18:15 +0000

    create the classes for reader a text file of mails (MailListReader) and the histogram of this emails(MailHistogramBuilder).

--- src/kata4/model/Mail.java
+++ src/kata4/model/Mail.java
@@ -1,5 +1,19 @@
 package kata4.model;
 
 public class Mail {
+    String mail;
 
+    public Mail(String mail) {
+        this.mail = mail;
+    }
+    
+    public String getDomain(){
+        return "";
+    }
+    
+    public static boolean isMail(String lineFile){
+        
+        return true;
+    }
+    
 }
